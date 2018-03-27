9d8fdab3bf9cbcd32c5d58a6d707bccb35e85bfd
Andrew Morris <andrew.morris03@mymona.uwi.edu>
2018-03-27 20:55:09 +0000

    First Lab 6 COmmit

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -39,6 +39,26 @@ Vue.component('app-footer', {
     }
 })
 
+Vue.component('news-list', {
+    template: `<div class ="news"> 
+    <h2>News</h2> 
+    <ul class ="news__list"> 
+        <li class="news__item">News item 1</li>
+        <li class="news__item">News item 2</li> 
+        <li class="news__item">News item 3</li> 
+        </ul> 
+        </div> 
+    `, created: function(){
+        let self=this; 
+        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=<your-api-key>').then(function (response){return response.json();}).then(function(data){console.log(data); self.articles=data.articles;}); 
+    }, data: function(){ 
+        return { 
+            articles :[] 
+        } 
+    }
+    
+});  
+
 
 let app = new Vue({
     el: '#app',
